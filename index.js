89eb70b18b0f261cc31efa99670350c2cfe94270
Sohan Karfa <112119230+Sohan-2001@users.noreply.github.com>
2024-06-13 00:40:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ document.getElementById('fileInput').addEventListener('change', function(event) 
     reader.readAsDataURL(file);
   });
   
+  function printSpecificElement() {
+    document.getElementById('middle').style.position = 'fixed';
+    var printContent = document.getElementById('PrintId').innerHTML;
+    var originalContent = document.body.innerHTML;
+
+    document.body.innerHTML = printContent;
+    window.print();
+    document.body.innerHTML = originalContent;
+    document.getElementById('middle').style.position = 'relative';
+}
