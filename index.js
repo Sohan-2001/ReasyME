73b2e65ad6746567edf1d168bc32ed85cba770e9
Sohan Karfa <112119230+Sohan-2001@users.noreply.github.com>
2024-06-14 14:13:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,27 @@ document.getElementById('fileInput').addEventListener('change', function(event) 
   });
   
   function printSpecificElement() {
+    var elementsNone = document.getElementsByClassName('None');
+    for (var i = 0; i < elementsNone.length; i++) {
+        elementsNone[i].style.resize = 'none';
+    }
     document.getElementById('middle').style.position = 'fixed';
     var printContent = document.getElementById('PrintId').innerHTML;
     var originalContent = document.body.innerHTML;
-
     document.body.innerHTML = printContent;
     window.print();
     document.body.innerHTML = originalContent;
     document.getElementById('middle').style.position = 'relative';
+    Resize();
+    
+}
+
+function Resize(){
+  var elementsNone = document.getElementsByClassName('None');
+    for (var i = 0; i < elementsNone.length; i++) {
+        elementsNone[i].style.resize = 'vertical';
+        elementsNone[i].style.overflow = 'auto'; // Necessary for the resize handle to app
+    }
+
+
 }
